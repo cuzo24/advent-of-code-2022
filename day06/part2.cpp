d4b140e76fb2eb5265a50cede1ee4cce6435621e
Carlos Zamora <czamora5@ucol.mx>
2022-12-05 23:14:38 -0600

    add day 6 solution

---
+++ day06/part2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char *argv[]) {
+  if (argc > 0) {
+    std::fstream file;
+    file.open(argv[1], std::ios::in);
+
+    if (file.is_open()) {
+      std::string input;
+      int len = 14;
+
+      while (std::getline(file, input)) {
+        for (int i = 0; i <= input.length() - len; i++) {
+          bool included[26] = {false}, done = true;
+          for (int j = 0; j < len; j++) {
+            if (included[input[i + j] - 'a']) {
+              done = false;
+              break;
+            } else included[input[i + j] - 'a'] = true;
+          }
+
+          if (done) {
+            std::cout << i + len << std::endl;
+            break;;
+          }
+        }
+      }
+      file.close();
+    }
+  }
+  return 0;
+}
